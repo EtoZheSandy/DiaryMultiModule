f8ed3a8a595f45ed5d6d19545077fd270ef3a98b
Alexander <82713915+EtoZheSandy@users.noreply.github.com>
2024-06-27 23:28:57 +0300

    Дизайн экрана авторизации

--- app/src/main/java/su/afk/diarymultimodule/navigation/NavGraph.kt
+++ app/src/main/java/su/afk/diarymultimodule/navigation/NavGraph.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.DialogNavigator
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import su.afk.diarymultimodule.presentation.screens.auth.AuthenticationScreen
 import su.afk.diarymultimodule.util.Constants.WRITE_SCREEN_ARG_KEY
 
 @Composable
@@ -29,7 +30,10 @@ fun SetupNavGraph(
 
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
-//        AuthenticationScreen()
+        AuthenticationScreen(
+            loadingState = false,
+            onButtonClicked = {}
+        )
     }
 }
 
---
+++ app/src/main/java/su/afk/diarymultimodule/presentation/screens/auth/AuthenticationScreen.kt
@@ -0,0 +1,23 @@
+package su.afk.diarymultimodule.presentation.screens.auth
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun AuthenticationScreen(
+    loadingState: Boolean,
+    onButtonClicked: () -> Unit
+) {
+    Scaffold(
+        content = {
+            AuthenticationContent(
+                loadingState = loadingState,
+                onButtonClicked = onButtonClicked
+            )
+        }
+    )
+}
